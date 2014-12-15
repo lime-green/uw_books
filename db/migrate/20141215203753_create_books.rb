cb992600f6eee885caac03e4600f22bb8441316b
Joshua Doncaster-Marsiglio <jmarsiglio@gmail.com>
2014-12-15 15:55:07 -0500

    Create book model migration. Strict non-null values only

---
+++ db/migrate/20141215203753_create_books.rb
@@ -0,0 +1,19 @@
+class CreateBooks < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+      t.string :author, null: false
+      t.string :title, null: false
+      t.integer :sku, null: false
+      t.float :price, null: false
+      t.integer :stock, null: false
+      t.integer :term, null: false
+      t.string :department, null: false
+      t.string :course, null: false
+      t.integer :section, null: false
+      t.string :instructor, null: false
+      t.boolean :reqopt, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,54 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20141215203753) do
+
+  create_table "books", force: true do |t|
+    t.string   "author",     null: false
+    t.string   "title",      null: false
+    t.integer  "sku",        null: false
+    t.float    "price",      null: false
+    t.integer  "stock",      null: false
+    t.integer  "term",       null: false
+    t.string   "department", null: false
+    t.string   "course",     null: false
+    t.integer  "section",    null: false
+    t.string   "instructor", null: false
+    t.boolean  "reqopt",     null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "delayed_jobs", force: true do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
+
+  create_table "tests", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
