f9ea720b5c09d7d5da15e82609b7d75b345ec5b7
Ryan De Villa <ryand@nulogy.com>
2014-12-15 18:16:00 -0500

    Strengthen spec

--- spec/repositories/book_repository_spec.rb
+++ spec/repositories/book_repository_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 describe BookRepository do
   it "finds all books for a given course" do
     expected_record = FactoryGirl.create :book, department: "CS", course: "777"
+    irrelevant_record = FactoryGirl.create :book, department: "AFM", course: "101"
     expect(BookRepository.find_by_course("CS", "777")).to eq([expected_record])
   end
 end
