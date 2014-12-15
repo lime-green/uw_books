a14daa7c5c8b1480a930a1557fd539d0baeea83c
Ryan De Villa <ryand@nulogy.com>
2014-12-15 09:29:05 -0500

    Use real HTML document in spec

--- lib/crawler/scraper.rb
+++ lib/crawler/scraper.rb
@@ -7,14 +7,13 @@ module Scraper
         title: book_node.css(".title").text,
         sku: book_node.css(".sku").text,
         price: book_node.css(".price").text,
-        stock: book_node.css(".stock").text,
-        term: book_node.at_xpath("//input[@name='mv_order_orderline_term']")[:value],
-        department: book_node.at_xpath("//input[@name='mv_order_orderline_department']")[:value],
-        course: book_node.at_xpath("//input[@name='mv_order_orderline_course']")[:value],
-        section: book_node.at_xpath("//input[@name='mv_order_orderline_section']")[:value],
-        instructor: book_node.at_xpath("//input[@name='mv_order_orderline_instructor']")[:value],
-        reqopt: book_node.at_xpath("//input[@name='mv_order_orderline_reqopt']")[:value],
-        quantity: book_node.at_xpath("//input[@name='mv_order_quantity']")[:value]
+        stock: book_node.css(".stock").text.gsub(/[\n\t]/, ""),
+        term: book_node.at_xpath("input[@name='mv_order_orderline_term']")[:value],
+        department: book_node.at_xpath("input[@name='mv_order_orderline_department']")[:value],
+        course: book_node.at_xpath("input[@name='mv_order_orderline_course']")[:value],
+        section: book_node.at_xpath("input[@name='mv_order_orderline_section']")[:value],
+        instructor: book_node.at_xpath("input[@name='mv_order_orderline_instructor']")[:value],
+        reqopt: book_node.at_xpath("input[@name='mv_order_orderline_reqopt']")[:value],
       }
     end
   end
--- spec/lib/crawler/scraper_spec.rb
+++ spec/lib/crawler/scraper_spec.rb
@@ -1,40 +1,153 @@
 require 'crawler/scraper'
+require 'nokogiri'
 describe Scraper do
 
   it "scrapes an HTML document for books" do
-    document = double("BookLook Document")
+    document = Nokogiri::HTML(File.open(File.dirname(__FILE__) + "/fixture.html"))
 
-    book_node = double("Book 1")
+    result = Scraper.scrape_page(document)
 
-    allow(book_node).to receive(:css).with(".author").and_return(double(text: "William Gibson"))
-    allow(book_node).to receive(:css).with(".title").and_return(double(text: "Neuromancer"))
-    allow(book_node).to receive(:css).with(".sku").and_return(double(text: "12345"))
-    allow(book_node).to receive(:css).with(".price").and_return(double(text: "$13.37"))
-    allow(book_node).to receive(:css).with(".stock").and_return(double(text: "42"))
-    allow(book_node).to receive(:at_xpath).with("//input[@name='mv_order_orderline_term']").and_return({value: "1151"})
-    allow(book_node).to receive(:at_xpath).with("//input[@name='mv_order_orderline_department']").and_return({value: "CS"})
-    allow(book_node).to receive(:at_xpath).with("//input[@name='mv_order_orderline_course']").and_return({value: "666"})
-    allow(book_node).to receive(:at_xpath).with("//input[@name='mv_order_orderline_section']").and_return({value: "001"})
-    allow(book_node).to receive(:at_xpath).with("//input[@name='mv_order_orderline_instructor']").and_return({value: "Ryan De Villa"})
-    allow(book_node).to receive(:at_xpath).with("//input[@name='mv_order_orderline_reqopt']").and_return({value: "R"})
-    allow(book_node).to receive(:at_xpath).with("//input[@name='mv_order_quantity']").and_return({value: "23"})
-
-    allow(document).to receive(:search).with("div.book_info").and_return([book_node])
-
-    expect(Scraper.scrape_page(document)).to eq([{
-      author: "William Gibson",
-      title: "Neuromancer",
-      sku: "12345",
-      price: "$13.37",
-      stock: "42",
-      term: "1151",
-      department: "CS",
-      course: "666",
+    expect(result.length).to eq(10)
+    expect(result).to include(hash_including({
+      author: "SHREVE S E",
+      title: "STOCHASTIC CALCULUS FOR FINANCE 1",
+      sku: "SKU: 9780387249681",
+      price: "Price: 62.00",
+      stock: "Estimated Copies Available:5",
+      term: "1149",
+      department: "ACC",
+      course: "770",
       section: "001",
-      instructor: "Ryan De Villa",
-      reqopt: "R",
-      quantity: "23"
-    }])
+      instructor: "Li,Bin",
+      reqopt: "R"
+    }));
+
+    expect(result).to include(hash_including({
+      author: "SHREVE",
+      title: "STOCHASTIC CALCULUS FOR FINANCE II",
+      sku: "SKU: 9780387401010",
+      price: "Price: 85.50",
+      stock: "Estimated Copies Available:2",
+      term: "1149",
+      department: "ACC",
+      course: "770",
+      section: "001",
+      instructor: "Li,Bin",
+      reqopt: "R"
+    }));
+
+    expect(result).to include(hash_including({
+      author: "LIPSON",
+      title: "DOING HONEST WORK IN COLLEGE 2ND ED",
+      sku: "SKU: 9780226484778",
+      price: "Price: 16.10",
+      stock: "Estimated Copies Available:0",
+      term: "1149",
+      department: "ACC",
+      course: "781",
+      section: "001",
+      instructor: "O'Brien,Patricia",
+      reqopt: "R"
+    }));
+
+    expect(result).to include(hash_including({
+      author: "SHADISH ET AL",
+      title: "EXPERIMENTAL & QUASI-EXPERIMENTAL DESIGNS 2ED",
+      sku: "SKU: 9780395615560",
+      price: "Price: 177.95",
+      stock: "Estimated Copies Available:0",
+      term: "1149",
+      department: "ACC",
+      course: "781",
+      section: "001",
+      instructor: "O'Brien,Patricia",
+      reqopt: "R"
+    }));
+
+    expect(result).to include(hash_including({
+      author: "TROCHIM & DONNELLY",
+      title: "RESEARCH METHODS KNOWLEDGE BASE 3ED",
+      sku: "SKU: 9781592602919",
+      price: "Price: 162.95",
+      stock: "Estimated Copies Available:0",
+      term: "1149",
+      department: "ACC",
+      course: "781",
+      section: "001",
+      instructor: "O'Brien,Patricia",
+      reqopt: "O"
+    }));
+
+    expect(result).to include(hash_including({
+      author: "BROWN ET AL",
+      title: "MATHEMATICS OF FINANCE 7ED",
+      sku: "SKU: 9780070000186",
+      price: "Price: 127.95",
+      stock: "Estimated Copies Available:1",
+      term: "1149",
+      department: "ACTSC",
+      course: "221",
+      section: "001",
+      instructor: "Blake,William Peter",
+      reqopt: "R"
+    }));
+
+    expect(result).to include(hash_including({
+      author: "BROWN & KOPP",
+      title: "FINANCIAL MATHEMATICS: THEORY & PRACTICE",
+      sku: "SKU: 9781259033803",
+      price: "Price: 87.95",
+      stock: "Estimated Copies Available:121",
+      term: "1149",
+      department: "ACTSC",
+      course: "231",
+      section: "001",
+      instructor: "Freeland,R Keith",
+      reqopt: "R"
+    }));
+
+    expect(result).to include(hash_including({
+      author: "DICKSON ET AL",
+      title: "ACTUARIAL MATHEMATICS FOR LIFE CONTINGENT RISKS 2ND",
+      sku: "SKU: 9781107044074",
+      price: "Price: 92.95",
+      stock: "Estimated Copies Available:38",
+      term: "1149",
+      department: "ACTSC",
+      course: "232",
+      section: "001",
+      instructor: "Adcock,James",
+      reqopt: "R"
+    }));
+
+    expect(result).to include(hash_including({
+      author: "DICKSON ET AL",
+      title: "SOLUTIONS MANUAL FOR 2ED OF ACTUARIAL MATHEMATICS FOR LIFE CONTINGENT RISKS 2ED",
+      sku: "SKU: 9781107620261",
+      price: "Price: 34.95",
+      stock: "Estimated Copies Available:41",
+      term: "1149",
+      department: "ACTSC",
+      course: "232",
+      section: "001",
+      instructor: "Adcock,James",
+      reqopt: "R"
+    }));
+
+    expect(result).to include(hash_including({
+      author: "DICKSON ET AL",
+      title: "ACTUARIAL MATHEMATICS FOR LIFE CONTINGENT RISKS 2ND",
+      sku: "SKU: 9781107044074",
+      price: "Price: 92.95",
+      stock: "Estimated Copies Available:38",
+      term: "1149",
+      department: "ACTSC",
+      course: "331",
+      section: "001",
+      instructor: "Hardy,Mary R",
+      reqopt: "R"
+    }));
+
   end
 
 
