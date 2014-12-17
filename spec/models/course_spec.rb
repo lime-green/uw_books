c49071132613cb2b497cced2f01b7bde4df1d3d9
Joshua Doncaster-Marsiglio <jmarsiglio@gmail.com>
2014-12-17 13:19:35 -0500

    Add course model test (for factory)

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Course, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    factory = FactoryGirl.create(:course)
+    expect(factory).to be_valid, lambda { factory.errors.full_messages.join("\n") }
+  end
+
+  it "allows no null values"
 end
