83fbdc3284929d0a5c6077fe669b854a58b4e153
Joshua Doncaster-Marsiglio <jmarsiglio@gmail.com>
2014-12-17 18:53:08 -0500

    Add model test for uppercase callback

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -6,5 +6,11 @@ RSpec.describe Course, :type => :model do
     expect(factory).to be_valid, lambda { factory.errors.full_messages.join("\n") }
   end
 
+  it "saves department as uppercase" do
+    FactoryGirl.create(:course, department: "mAtH")
+    saved = Course.first
+    expect(saved[:department]).to eq("MATH")
+  end
+
   it "allows no null values"
 end
