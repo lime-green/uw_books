d12b62f556e408d0174b7de3d76d400013910206
Ryan DV <rdevilla@uwaterloo.ca>
2014-12-19 20:12:23 -0500

    Improve interface to Crawler
    Crawler now returns a collection of books found.

--- lib/crawler/crawler.rb
+++ lib/crawler/crawler.rb
@@ -1,33 +1,32 @@
 require_relative 'scraper'
 require 'digest'
+require 'mechanize'
+require 'net/http'
+
 class Crawler
-  require 'mechanize'
-  require 'net/http'
   ROOT_URL = "https://fortuna.uwaterloo.ca/cgi-bin/cgiwrap/rsic/book"
   CURRENT_TERM = "1149" # calculate this?
 
-  VERBOSE = false # useful messages
+  PAGE_CONNECTION_ATTEMPT_MAX = 5
+
+  VERBOSE = true # useful messages
 
   SLICE_MAX = 5         # crawl this amount of links asynchronously
   RETRY_DELAY = 10      # sleep for this amount of seconds after a failed request
 
   def initialize
-
-    @output = []
-
-    # Prepare page
     @agent = Mechanize.new
-    @root_page = @agent.get(ROOT_URL)
-    target_form = @root_page.at('div#search_box_course form') # Nokogiri object
-    target_form = Mechanize::Form.new( target_form )          # Convert back to Mechanize object
-    target_form.field_with(value: /[0-9]{4}/).option_with(value: CURRENT_TERM).click # Select the term
-    @root_page = @agent.submit(target_form) # first page of results
   end
+
+  def get_books
+    crawl
+  end
+
+  private
 
   def get_page(link, thread_name)
     attempt = 1
-    attempt_max = 5
-    while attempt <= attempt_max do
+    while attempt <= PAGE_CONNECTION_ATTEMPT_MAX do
       begin
         page = @agent.get(link.href)
         break
@@ -41,13 +40,16 @@ class Crawler
         end
       end
     end
-    raise "Over #{attempt_max} attempts made for #{link.href}, exiting" if attempt > attempt_max
+    raise "Over #{PAGE_CONNECTION_ATTEMPT_MAX} attempts made for #{link.href}, exiting" if attempt > PAGE_CONNECTION_ATTEMPT_MAX
     return page
   end
 
-  def crawl!
+  def crawl
+    root_page = @agent.get(ROOT_URL)
+    first_page = submit_form(root_page)
+
     # Retrieve all links to paginated results. As at Dec 10/2014 there are 301 pages for term 1149
-    links = @root_page.links_with(href: /book\/scan/).uniq { |link| link.href }
+    links = first_page.links_with(href: /book\/scan/).uniq { |link| link.href }
 
     threads = []
     num_threads = 1
@@ -55,7 +57,7 @@ class Crawler
     books = Queue.new
 
     threads << Thread.new do
-      add_books(books, @root_page)
+      add_books(books, first_page)
     end
 
     # for now, scrape only 10 links (2 slices of 5)
@@ -72,37 +74,20 @@ class Crawler
       print_verbose "Done scraping that set"
     end
 
-    # save all this data
-    write_DB!(books)
-  end # end crawl! function
+    result = []
 
-  def get_characterization
-    @output.sort! do |x,y|
-      sku_comparison = x[:sku] <=> y[:sku]
-      if sku_comparison == 0
-        instructor_comparison = x[:instructor] <=> y[:instructor]
-        if instructor_comparison == 0
-          course_comparison = x[:course] <=> y[:course]
-          if course_comparison == 0
-            x[:section] <=> y[:section]
-          else
-            course_comparison
-          end
-        else
-          instructor_comparison
-        end
-      else
-        sku_comparison
-      end
+    while books.length > 0 do
+      result << books.pop
     end
 
-    Digest::SHA256.hexdigest @output.to_s
+    result
   end
 
-  # ALL PRIVATE FUNCTIONS FOLLOW
-  private
-  def print_verbose(string)
-    print "====Crawler(verbose)====\n#{string}\n====END verbose====\n\n" if VERBOSE
+  def submit_form(root_page)
+    target_form = root_page.at('div#search_box_course form') # Nokogiri object
+    target_form = Mechanize::Form.new( target_form )          # Convert back to Mechanize object
+    target_form.field_with(value: /[0-9]{4}/).option_with(value: CURRENT_TERM).click # Select the term
+    @agent.submit(target_form) # first page of results
   end
 
   def add_books(books, document)
@@ -111,11 +96,7 @@ class Crawler
     end
   end
 
-  def write_DB!(records)
-    print_verbose "Writing to database"
-    while records.length > 0 do
-      # Book.create(row) or Book.update_attributes(row) if it exists
-      @output << records.pop
-    end
-  end # end write_DB function
-end # end class
+  def print_verbose(string)
+    print "====Crawler(verbose)====\n#{string}\n====END verbose====\n\n" if VERBOSE
+  end
+end
--- spec/lib/crawler/crawler_characterization_spec.rb
+++ spec/lib/crawler/crawler_characterization_spec.rb
@@ -1,10 +1,36 @@
 require_relative '../../../lib/crawler/crawler'
+require 'digest'
 
 describe Crawler do
   let (:expected_digest) { "dfe0e42e1e2840e835d2124e307b83eb7b773ec55fe483c37bc1df532c143cc8" }
+
   it "crawls the BookLook site and creates records for all books found" do
     crawler = Crawler.new
-    crawler.crawl!
-    expect(crawler.get_characterization).to eq(expected_digest)
+    expect(get_characterization(crawler.get_books)).to eq(expected_digest)
+  end
+
+  private
+
+  def get_characterization(output)
+    output.sort! do |x,y|
+      sku_comparison = x[:sku] <=> y[:sku]
+      if sku_comparison == 0
+        instructor_comparison = x[:instructor] <=> y[:instructor]
+        if instructor_comparison == 0
+          course_comparison = x[:course] <=> y[:course]
+          if course_comparison == 0
+            x[:section] <=> y[:section]
+          else
+            course_comparison
+          end
+        else
+          instructor_comparison
+        end
+      else
+        sku_comparison
+      end
+    end
+
+    Digest::SHA256.hexdigest output.to_s
   end
 end
